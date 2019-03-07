8d92ada45e9e3d81e5be80937ca94e5860be2c75
divsh <divsh23@gmail.com>
2019-03-08 12:07:53 +1300

    First working copy

---
+++ fewestComparisonSort/fewestComparisonSort/Form1.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lblComparisonCount = New System.Windows.Forms.Label()
+        Me.lblTotalItems = New System.Windows.Forms.Label()
+        Me.btnA = New System.Windows.Forms.Button()
+        Me.btnB = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.txtList = New System.Windows.Forms.TextBox()
+        Me.txtSortedList = New System.Windows.Forms.TextBox()
+        Me.btnEqual = New System.Windows.Forms.Button()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(612, 293)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lblComparisonCount
+        '
+        Me.lblComparisonCount.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lblComparisonCount.AutoSize = True
+        Me.lblComparisonCount.ForeColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.lblComparisonCount.Location = New System.Drawing.Point(588, 416)
+        Me.lblComparisonCount.Name = "lblComparisonCount"
+        Me.lblComparisonCount.Size = New System.Drawing.Size(73, 13)
+        Me.lblComparisonCount.TabIndex = 3
+        Me.lblComparisonCount.Text = "comparisons#"
+        Me.lblComparisonCount.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblTotalItems
+        '
+        Me.lblTotalItems.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lblTotalItems.AutoSize = True
+        Me.lblTotalItems.ForeColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.lblTotalItems.Location = New System.Drawing.Point(504, 416)
+        Me.lblTotalItems.Name = "lblTotalItems"
+        Me.lblTotalItems.Size = New System.Drawing.Size(59, 13)
+        Me.lblTotalItems.TabIndex = 4
+        Me.lblTotalItems.Text = "Total Items"
+        '
+        'btnA
+        '
+        Me.btnA.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnA.Location = New System.Drawing.Point(12, 322)
+        Me.btnA.Name = "btnA"
+        Me.btnA.Size = New System.Drawing.Size(675, 23)
+        Me.btnA.TabIndex = 5
+        Me.btnA.Text = "OptionA"
+        Me.btnA.UseVisualStyleBackColor = True
+        '
+        'btnB
+        '
+        Me.btnB.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnB.Location = New System.Drawing.Point(12, 351)
+        Me.btnB.Name = "btnB"
+        Me.btnB.Size = New System.Drawing.Size(675, 23)
+        Me.btnB.TabIndex = 6
+        Me.btnB.Text = "OptionB"
+        Me.btnB.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 296)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Select the "
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(69, 293)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 8
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(175, 292)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(391, 26)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "(Specify the comparitive word. For e.g. greater, longer, lower, more, lesser, bet" & _
+    "ter)." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "That will move to top."
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.SplitContainer1.Location = New System.Drawing.Point(12, 12)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.txtList)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.txtSortedList)
+        Me.SplitContainer1.Size = New System.Drawing.Size(675, 275)
+        Me.SplitContainer1.SplitterDistance = 327
+        Me.SplitContainer1.TabIndex = 10
+        '
+        'txtList
+        '
+        Me.txtList.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtList.Location = New System.Drawing.Point(0, 0)
+        Me.txtList.Multiline = True
+        Me.txtList.Name = "txtList"
+        Me.txtList.Size = New System.Drawing.Size(327, 275)
+        Me.txtList.TabIndex = 1
+        '
+        'txtSortedList
+        '
+        Me.txtSortedList.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtSortedList.Location = New System.Drawing.Point(0, 0)
+        Me.txtSortedList.Multiline = True
+        Me.txtSortedList.Name = "txtSortedList"
+        Me.txtSortedList.Size = New System.Drawing.Size(344, 275)
+        Me.txtSortedList.TabIndex = 3
+        '
+        'btnEqual
+        '
+        Me.btnEqual.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnEqual.Location = New System.Drawing.Point(12, 380)
+        Me.btnEqual.Name = "btnEqual"
+        Me.btnEqual.Size = New System.Drawing.Size(675, 23)
+        Me.btnEqual.TabIndex = 11
+        Me.btnEqual.Text = "Equal"
+        Me.btnEqual.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(699, 438)
+        Me.Controls.Add(Me.btnEqual)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnB)
+        Me.Controls.Add(Me.btnA)
+        Me.Controls.Add(Me.lblTotalItems)
+        Me.Controls.Add(Me.lblComparisonCount)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Arrange Quickly"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.PerformLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents lblComparisonCount As System.Windows.Forms.Label
+    Friend WithEvents lblTotalItems As System.Windows.Forms.Label
+    Friend WithEvents btnA As System.Windows.Forms.Button
+    Friend WithEvents btnB As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+    Friend WithEvents txtList As System.Windows.Forms.TextBox
+    Friend WithEvents txtSortedList As System.Windows.Forms.TextBox
+    Friend WithEvents btnEqual As System.Windows.Forms.Button
+
+End Class
---
+++ fewestComparisonSort/fewestComparisonSort/Form1.vb
@@ -0,0 +1,279 @@
+﻿Imports System.Linq
+
+Public Class Form1
+
+
+    Private mComparisonsCount As Integer
+    Dim prompt As Prompt
+    Private mEqualItemsIndex As Short
+    Private Property mSortInprogress As Boolean = False
+
+    Public Property ComparisonsCount() As Integer
+        Get
+            Return mComparisonsCount
+        End Get
+        Set(ByVal value As Integer)
+            mComparisonsCount = value
+            lblComparisonCount.Text = "Comparisons# :" & mComparisonsCount.ToString
+        End Set
+    End Property
+
+    Private mTotalItems As Integer
+    Public Property TotalItems() As Integer
+        Get
+            Return mTotalItems
+        End Get
+        Set(ByVal value As Integer)
+            mTotalItems = value
+            lblTotalItems.Text = "Total Items: " & mTotalItems.ToString
+        End Set
+    End Property
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Debug.WriteLine("*************Starting**************")
+        Dim SortingItems As String()
+        Dim testInt As Integer
+        Dim lstString As String()
+
+        mEqualItemsIndex = 0
+        ComparisonsCount = 0
+        SortingItems = txtList.Lines.ToList().ToArray
+        If SortingItems.Any(Function(x) String.IsNullOrEmpty(x)) Then
+            MessageBox.Show("There are some blank lines." & vbNewLine & "Please remove them and start again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        'lstNumber = txtList.Lines.ToList() _
+        '.Where(Function(ln) Integer.TryParse(ln.Trim(), testInt)) _
+        '.Select(Function(ln) Convert.ToInt32(ln.Trim())).ToArray()
+
+        TotalItems = SortingItems.Count
+        Try
+            'mergeSort(lstNumber, 0, lstNumber.Count - 1)
+            QuickSort(SortingItems, 0, SortingItems.Count - 1)
+        Catch ex As Exception
+            MessageBox.Show(ex.StackTrace)
+        End Try
+        lstString = SortingItems.Select(Function(ln) Convert.ToString(ln) & Environment.NewLine).ToArray()
+        txtSortedList.Text = listToString(lstString.ToList)
+    End Sub
+
+    Private Function listToString(lst As List(Of String), Optional seperator As String = "") As String
+        listToString = String.Empty
+        For Each item As String In lst
+            listToString &= item
+        Next
+    End Function
+
+    Public Sub mergeSort(arr As Integer(), l As Integer, r As Integer)
+        If r - l = 1 Then
+            ComparisonsCount += 1
+            If arr(l) > arr(r) Then
+                Dim tmp As Integer = arr(r)
+                arr(r) = arr(l)
+                arr(l) = tmp
+
+            End If
+            Return
+        End If
+        If l = r Then
+            Return
+        End If
+        If l < r Then
+            Dim m As Integer = l + (r - l) / 2
+            printarray(arr, False, l, m)
+            Debug.Write("|")
+            printarray(arr, False, m + 1, r)
+            Debug.Write(Environment.NewLine)
+            mergeSort(arr, l, m)
+            mergeSort(arr, m + 1, r)
+
+
+            merge(arr, l, m, r)
+            printarray(arr, True, l, r)
+        End If
+    End Sub
+
+
+    ''' <summary>
+    ''' 2 sorted lists (l..m) and (m+1..r) are being merged
+    ''' </summary>
+    Private Sub merge(arr As Integer(), l As Integer, m As Integer, r As Integer)
+        Dim mLeft As Integer
+        Dim insertPos As Integer
+        'insert lst(i) between l and m inclusive
+        For i As Integer = m + 1 To r
+            mLeft = l + (m - l) / 2
+            insertPos = positionInSortedList(arr, l, i - 1, arr(i))
+            Dim shiftingItem As Integer = arr(i)
+            For d As Integer = i - 1 To insertPos Step -1
+                If d < 0 Then
+                    Exit For
+                End If
+                arr(d + 1) = arr(d)
+                'printarray(arr, True)
+            Next
+            arr(insertPos) = shiftingItem
+            'printarray(arr, True, l, r)
+        Next
+    End Sub
+
+    Private Function positionInSortedList(arr As Integer(), l As Integer, r As Integer, NewMember As Integer) As Integer
+
+        Debug.WriteLine("l={0};r={1};newMember={2}", l, r, NewMember)
+        Dim m As Integer
+        m = l + (r - l) / 2
+
+        If m = l Then
+
+            If NewMember < arr(m) Then
+                ComparisonsCount += 1
+                Return l
+            ElseIf NewMember < arr(r) Then
+                ComparisonsCount += 1
+                Return r
+            Else
+                Return r + 1
+            End If
+        ElseIf m = r Then
+            If NewMember < arr(l) Then
+                ComparisonsCount += 1
+                Return l
+            ElseIf NewMember < arr(r) Then
+                ComparisonsCount += 1
+                Return r
+            Else
+                Return r + 1
+            End If
+        End If
+
+        'If NewMember = arr(m) Then
+        '    Return m + 1
+        'ElseIf NewMember < arr(m) Then
+        '    Return positionInSortedList(arr, l, m, NewMember)
+        'Else
+        '    Return positionInSortedList(arr, m + 1, r, NewMember)
+        'End If
+        ComparisonsCount += 1
+        If NewMember < arr(m) Then
+            Return positionInSortedList(arr, l, m, NewMember)
+        Else
+            Return positionInSortedList(arr, m + 1, r, NewMember)
+        End If
+
+    End Function
+
+    Sub printarray(arr As Integer(), insertNewLine As Boolean, Optional l As Integer = -1, Optional r As Integer = -1)
+        If r = -1 Then
+            r = arr.Count - 1
+        End If
+        If l = -1 Then
+            l = 0
+        End If
+        For i As Integer = l To r
+            Debug.Write(arr(i).ToString & ",")
+        Next
+        If insertNewLine Then Debug.Write(Environment.NewLine)
+    End Sub
+
+    Private Sub QuickSort(arr As String(), l As Integer, r As Integer)
+        mSortInprogress = True
+        If r - l = 0 Then
+            Return
+        End If
+
+        'If r - l <= 0 Then
+        '    Return
+        'ElseIf r - l <= 1 Then
+        '    If arr(l) > arr(r) Then
+        '        Dim tmp As Integer = arr(l)
+        '        arr(l) = arr(r)
+        '        arr(r) = tmp
+        '    End If
+        '    Return
+        'End If
+
+        Dim pivotIndex As Integer = l
+        Dim elementQueried As String
+        For i As Integer = pivotIndex + 1 To r
+            ComparisonsCount += 1
+            If prompt.showPrompt(arr(i), arr(pivotIndex)) > 0 Then
+                'If arr(i) <= arr(pivotIndex) Then
+                'shift pivotindex till i-1 elements to next index
+                'move arr(i) at pivotindex
+                elementQueried = arr(i)
+                For d As Integer = i To pivotIndex + 1 Step -1
+                    arr(d) = arr(d - 1)
+                Next
+                arr(pivotIndex + 1) = arr(pivotIndex)
+                arr(pivotIndex) = elementQueried
+                pivotIndex += 1
+            End If
+        Next
+        If pivotIndex > l Then
+            QuickSort(arr, l, pivotIndex - 1)
+        End If
+        If pivotIndex < r Then
+            QuickSort(arr, pivotIndex + 1, r)
+        End If
+        mSortInprogress = False
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        prompt = New Prompt(btnA, btnB, btnEqual)
+    End Sub
+End Class
+
+
+Class Prompt
+    Private Shared WithEvents mbtnA, mbtnB, mbtnEqual As Button
+    Private Shared mUserResponded As Boolean
+    Private Shared mPromptResult As Short
+    Private Shared mEqualItemIndex As Integer = 0
+
+    Sub New(btnA As Button, btnB As Button, btnEqual As Button)
+        mbtnA = btnA
+        mbtnB = btnB
+        mbtnEqual = btnEqual
+    End Sub
+    Shared Function showPrompt(ByRef itemL As String, ByRef itemR As String) As Short
+        mbtnA.Text = itemL
+        mbtnB.Text = itemR
+        mUserResponded = False
+        While Not mUserResponded
+            Application.DoEvents()
+        End While
+        If mPromptResult = 0 Then
+            'if either has a number, use it to assign other AND dont increment index
+            If itemL.Substring(0, 1) = "(" Then
+                mEqualItemIndex = Integer.Parse(itemL.Substring(1, 1))
+                itemR = "(" & mEqualItemIndex.ToString & ")" & itemR
+            ElseIf itemR.Substring(0, 1) = "(" Then
+                mEqualItemIndex = Integer.Parse(itemR.Substring(1, 1))
+                itemL = "(" & mEqualItemIndex.ToString & ")" & itemL
+            Else
+                mEqualItemIndex += 1
+                itemL = "(" & mEqualItemIndex.ToString & ")" & itemL
+                itemR = "(" & mEqualItemIndex.ToString & ")" & itemR
+            End If
+        End If
+        Return mPromptResult
+    End Function
+
+    Private Shared Sub btnA_Click(sender As Object, e As EventArgs) Handles mbtnA.Click
+        mUserResponded = True
+        mPromptResult = 1
+    End Sub
+
+    Private Shared Sub btnB_Click(sender As Object, e As EventArgs) Handles mbtnB.Click
+        mUserResponded = True
+        mPromptResult = -1
+    End Sub
+    Private Shared Sub btnEqual_Click(sender As Object, e As EventArgs) Handles mbtnEqual.Click
+        mUserResponded = True
+        mPromptResult = 0
+    End Sub
+End Class
+
+
